9f8ec1085775e8449e7454fa812bdbec8ec29056
paul <syrovatskyi93@gmail.com>
2020-12-20 23:06:23 +0200

    axios is installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data)
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className="App">
-      Hello Crypto Tracker
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input 
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+          />
+        </form>
+      </div>
+      
     </div>
   );
 }
